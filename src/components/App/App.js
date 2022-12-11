cd5166bbea31526e22ceb40cd370d94da047b82b
Volkus <volkus80@gmail.com>
2022-12-11 20:17:50 +0200

    17

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import TasksPage from '../../pages/TasksPage/TasksPage';
 import ProjectsPage from '../../pages/ProjectsPage/ProjectsPage';
@@ -8,7 +8,7 @@ import {useSelector} from 'react-redux';
 
 function App() {
   const data = JSON.stringify(useSelector(state => state.data));
-  localStorage.setItem('todoData', data);
+  useEffect(() => localStorage.setItem('todoData', data),[data]);
    
   return (
     <Routes>
